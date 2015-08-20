abf0f6126d581528383600efc403b8b6a6a55543
Jens Walter <js.walter@gmx.net>
2015-08-20 19:46:37 +0200

    more license info

--- src/io/trivium/webui/EditorRequestHandler.java
+++ src/io/trivium/webui/EditorRequestHandler.java
@@ -1,10 +1,23 @@
+/*
+ * Copyright 2015 Jens Walter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.trivium.webui;
 
-import io.trivium.Central;
 import io.trivium.glue.binding.http.Session;
-import io.trivium.Central;
 import io.trivium.anystore.ObjectRef;
-import io.trivium.glue.binding.http.Session;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.nio.protocol.BasicAsyncRequestConsumer;
