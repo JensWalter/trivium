b9e23e7a6befa3fb81599181e0a19335ee10ed0d
Jens Walter <js.walter@gmx.net>
2015-08-20 19:54:59 +0200

    remove escaped code

--- src/io/trivium/TriviumLoader.java
+++ src/io/trivium/TriviumLoader.java
@@ -34,57 +34,22 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.*;
 
-/**
- * Our custom implementation of the ClassLoader.
- * For any of classes from "javablogging" package
- * it will use its {@link TriviumLoader#getClass()}
- * method to load it from the specific .class file. For any
- * other class it will use the super.loadClass() method
- * from ClassLoader, which will eventually pass the
- * request to the parent.
- *
- */
 public class TriviumLoader extends ClassLoader {
 
     private FastMap<String,Class<?>> classes = new FastMap<String,Class<?>>();
-    /**
-     * Parent ClassLoader passed to this constructor
-     * will be used if this ClassLoader can not resolve a
-     * particular class.
-     *
-     * @param parent Parent ClassLoader
-     *              (may be from getClass().getClassLoader())
-     */
+
     public TriviumLoader(ClassLoader parent) {
         super(parent);
     }
 
-    /**
-     * Loads a given class from .class file just like
-     * the default ClassLoader. This method could be
-     * changed to load the class over network from some
-     * other server or from the database.
-     *
-     * @param name Full class name
-     */
-    private Class<?> getClass(String name)
-            throws ClassNotFoundException {
-        // We are getting a name that looks like
-        // javablogging.package.ClassToLoad
-        // and we have to convert it into the .class file name
-        // like javablogging/package/ClassToLoad.class
-        try {
-//            Central.logger.debug("TriviumLoader getClass '{}'", name);
-        }catch(Exception ex){}
-        String file = name.replace('.', File.separatorChar)
-                + ".class";
+    private Class<?> getClass(String name) throws ClassNotFoundException {
+        String file = name.replace('.', File.separatorChar) + ".class";
         byte[] b = null;
         try {
             // This loads the byte code data from the file
             b = loadClassData(file);
             if((b==null || b.length==0 ) && Central.isRunning){
                 //load from anystore
-//                Central.logger.debug("loading class from anystore '{}'",name);
                 Query query = new Query();
                 query.criteria.add(new Value("canonicalName", name));
                 query.criteria.add(new Value("typeId", TypeIds.FILE.toString()));
@@ -93,7 +58,6 @@ public class TriviumLoader extends ClassLoader {
                 FileTypeFactory factory = new FileTypeFactory();
                 for(TriviumObject po : objects){
                     FileType memFile = factory.getInstance(po);
-//                    Central.logger.info("found class {}",memFile.metadata.findValue("canonicalName"));
                     b = Base64.getDecoder().decode(memFile.data);
                 }
             }
@@ -110,9 +74,6 @@ public class TriviumLoader extends ClassLoader {
 
     @Override
     public URL getResource(String name) {
-        try{
-//            Central.logger.debug("TriviumLoader getResource '{}'",name);
-        }catch(Exception ex){}
         return super.getResource(name);
     }
 
@@ -125,7 +86,6 @@ public class TriviumLoader extends ClassLoader {
         }
         if(name.startsWith("META-INF/services/")){
             //enrich with anystore
-//            Central.logger.debug("loading class from anystore '{}'",name);
             Query query = new Query();
             query.criteria.add(new Value("name", name));
             query.criteria.add(new Value("typeId", TypeIds.FILE.toString()));
@@ -134,38 +94,23 @@ public class TriviumLoader extends ClassLoader {
             for(TriviumObject po : objects){
                 String uri = "anystore://"+po.getId().toString();
                 URL url = new URL(uri);
-//                Central.logger.debug("adding url '{}'",uri);
                 result.add(url);
             }
         }
-        try{
-//            Central.logger.debug("TriviumLoader getResources '{}'",name);
-        }catch(Exception ex){}
         return result.elements();
     }
 
     @Override
     public InputStream getResourceAsStream(String name) {
-        try{
-//            Central.logger.debug("TriviumLoader getResourceAsStream '{}'",name);
-        }catch(Exception ex){}
         return super.getResourceAsStream(name);
     }
 
-    public static Enumeration<URL> getSystemResources(String name)
-            throws IOException
-    {
-        try{
-//            Central.logger.debug("TriviumLoader getSystemResources '{}'",name);
-        }catch(Exception ex){}
+    public static Enumeration<URL> getSystemResources(String name) throws IOException {
         return ClassLoader.getSystemResources(name);
     }
 
-    public Class<?> fromBytes(String name, byte[] input){
+    public Class<?> fromBytes(String name, byte[] input) {
         try{
-            try{
-//            Central.logger.debug("TriviumLoader fromBytes '{}'",name);
-            }catch(Exception ex){}
             Class<?> c = defineClass(name,input,0,input.length);
             return c;
         }catch(Exception ex){
@@ -174,22 +119,10 @@ public class TriviumLoader extends ClassLoader {
         }
     }
 
-    /**
-     * Every request for a class passes through this method.
-     * If the requested class is in "javablogging" package,
-     * it will load it using the
-     * {@link TriviumLoader#getClass()} method.
-     * If not, it will use the super.loadClass() method
-     * which in turn will pass the request to the parent.
-     *
-     * @param name
-     *            Full class name
-     */
     @Override
     public Class<?> loadClass(String name)
             throws ClassNotFoundException {
         try{
-//        Central.logger.debug("TriviumLoader loadClass '{}'",name);
             //look up anystore
             if(Central.isRunning) {
                 if(classes.containsKey(name)){
@@ -201,14 +134,11 @@ public class TriviumLoader extends ClassLoader {
                 query.criteria.add(new Value("contentType","application/java-vm"));
                 FastList<TriviumObject> objects = AnyClient.INSTANCE.loadObjects(query);
                 FileTypeFactory factory = new FileTypeFactory();
-//                Central.logger.info("found {} entries",objects.size());
                 for(TriviumObject po : objects){
                     FileType file = factory.getInstance(po);
-//                    Central.logger.info("found {} {}",file.name,file.contentType);
                     if(file.contentType.equals(ContentTypes.getMimeType("class"))
                             && file.name.replace('/','.').equals(name + ".class")) {
                         byte[] bytes = Base64.getDecoder().decode(file.data);
-//                        Central.logger.debug("TriviumLoader returning class definition '{}'", name);
                         Class<?> c= defineClass(name, bytes, 0, bytes.length);
                         classes.put(name,c);
                         return c;
@@ -216,23 +146,12 @@ public class TriviumLoader extends ClassLoader {
                 }
             }
         }catch(Exception ex){}
-       // System.out.println("loading class '" + name + "'");
         if (name.startsWith("javablogging.")) {
             return getClass(name);
         }
         return super.loadClass(name);
     }
 
-    /**
-     * Loads a given file (presumably .class) into a byte array.
-     * The file should be accessible as a resource, for example
-     * it could be located on the classpath.
-     *
-     * @param name File name to load
-     * @return Byte array read from the file
-     * @throws IOException Is thrown when there
-     *               was some problem reading the file
-     */
     private byte[] loadClassData(String name) throws IOException {
         // Opening the file
         InputStream stream = getClass().getClassLoader()
