4de43b3ac99ff01c85d5d7e42a68021f0fe7f17f
Jens Walter <js.walter@gmx.net>
2016-02-25 15:30:05 +0800

    just updated the copyright year

--- src/io/trivium/anystore/statics/MimeTypes.java
+++ src/io/trivium/anystore/statics/MimeTypes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Jens Walter
+ * Copyright 2016 Jens Walter
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
