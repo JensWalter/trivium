d01e1dcf15c23230b98887a56522768367f57683
Jens Walter <js.walter@gmx.net>
2016-03-06 13:19:20 +0800

    fixes #11 implementing atomic value parser/serialiser

--- src/io/trivium/extension/Fact.java
+++ src/io/trivium/extension/Fact.java
@@ -17,11 +17,13 @@
 package io.trivium.extension;
 
 import io.trivium.anystore.TypeRef;
-import io.trivium.extension.Typed;
+import io.trivium.dep.com.google.common.util.concurrent.AtomicDouble;
 import io.trivium.extension.fact.TriviumObject;
 import io.trivium.glue.om.Element;
 
 import java.lang.reflect.Field;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 
@@ -57,6 +59,12 @@ public interface Fact extends Typed {
                         field.setChar(this, el.getFirstChild(name).getValue().charAt(0));
                     } else if (field.getType() == AtomicLong.class) {
                         field.set(this, new AtomicLong(Long.parseLong(el.getFirstChild(name).getValue())));
+                    } else if (field.getType() == AtomicInteger.class) {
+                        field.set(this, new AtomicInteger(Integer.parseInt(el.getFirstChild(name).getValue())));
+                    } else if (field.getType() == AtomicBoolean.class) {
+                        field.set(this, new AtomicBoolean(Boolean.parseBoolean(el.getFirstChild(name).getValue())));
+                    } else if (field.getType() == AtomicDouble.class) {
+                        field.set(this, new AtomicDouble(Double.parseDouble(el.getFirstChild(name).getValue())));
                     } else {
                         //try String
                         field.set(this, el.getFirstChild(name).getValue());
@@ -84,7 +92,8 @@ public interface Fact extends Typed {
                 if (field.getType() == long.class || field.getType() == int.class
                         || field.getType() == boolean.class || field.getType() == byte.class || field.getType() == float.class
                         || field.getType() == double.class || field.getType() == short.class || field.getType() == char.class
-                        || field.getType() == String.class) {
+                        || field.getType() == String.class || field.getType() == AtomicLong.class || field.getType() == AtomicBoolean.class
+                        || field.getType() == AtomicInteger.class || field.getType() == AtomicDouble.class) {
                     Object obj = field.get(this);
                     if(obj != null) {
                         root.addChild(new Element(name, obj.toString()));
